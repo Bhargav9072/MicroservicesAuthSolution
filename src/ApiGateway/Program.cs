bc51a1a28da663c767b2f5db16e20dc146e50c06
Bhargav9072 <55135498+Bhargav9072@users.noreply.github.com>
2026-08-25 17:26:32 +0530

    Update Program.cs

--- src/ApiGateway/Program.cs
+++ src/ApiGateway/Program.cs
@@ -8,17 +8,14 @@ builder.Services.AddReverseProxy()
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(options =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(options =>
-    {
-        options.SwaggerEndpoint("/swagger/v1/swagger.json", "ApiGateway v1");
-        options.SwaggerEndpoint("/auth/swagger/v1/swagger.json", "AuthService v1 (via Gateway)");
-        options.SwaggerEndpoint("/users/swagger/v1/swagger.json", "UserService v1 (via Gateway)");
-        options.SwaggerEndpoint("/project/swagger/v1/swagger.json", "ProjectService v1 (via Gateway)");
-    });
-}
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "ApiGateway v1");
+    options.SwaggerEndpoint("/auth/swagger/v1/swagger.json", "AuthService v1 (via Gateway)");
+    options.SwaggerEndpoint("/users/swagger/v1/swagger.json", "UserService v1 (via Gateway)");
+    options.SwaggerEndpoint("/project/swagger/v1/swagger.json", "ProjectService v1 (via Gateway)");
+});
 
 app.MapReverseProxy();
 
